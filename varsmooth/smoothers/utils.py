98690bbc162482b5b09c74f2b95fd6088b1ba09e
hanyas <hanyas@users.noreply.github.com>
2024-08-09 14:33:31 +0300

    refactor reverse-forward initialization

--- varsmooth/smoothers/utils.py
+++ varsmooth/smoothers/utils.py
@@ -1,5 +1,7 @@
 from typing import Callable
+from functools import partial
 
+import jax
 from jax import numpy as jnp
 from jax import scipy as jsc
 
@@ -10,6 +12,8 @@ from varsmooth.objects import (
     LogPrior,
     LogTransition,
     LogObservation,
+    Potential,
+    LogConditionalNorm
 )
 from varsmooth.utils import none_or_idx, none_or_shift
 
@@ -25,6 +29,7 @@ def kl_between_marginals(p, q):
     )
 
 
+@partial(jax.jit, static_argnums=(1, 2, 3))
 def statistical_expansion(
     observations: jnp.ndarray,
     log_prior_fn: Callable,
@@ -45,45 +50,102 @@ def statistical_expansion(
 
 
 def initialize_reverse_with_forward(
-    forward_markov: GaussMarkov,
-    forward_marginals: Gaussian,
+    forward_markov: GaussMarkov
 ):
+    from varsmooth.smoothers.forward_markov import forward_std_message
 
-    rvs_Fs = jnp.zeros_like(forward_markov.kernels.F)
-    rvs_ds = jnp.zeros_like(forward_markov.kernels.d)
-    rvs_Sigmas = jnp.zeros_like(forward_markov.kernels.Sigma)
+    forward_marginals = forward_std_message(forward_markov)
 
-    nb_steps = rvs_Fs.shape[0]
+    Fs = jnp.zeros_like(forward_markov.kernels.F)
+    ds = jnp.zeros_like(forward_markov.kernels.d)
+    Sigmas = jnp.zeros_like(forward_markov.kernels.Sigma)
+
+    nb_steps = forward_markov.kernels.F.shape[0]
     for k in range(nb_steps):
-        m = forward_marginals.mean[k]
-        P = forward_marginals.cov[k]
 
-        fwd_F = forward_markov.kernels.F[k]
-        fwd_d = forward_markov.kernels.d[k]
-        fwd_Sigma = forward_markov.kernels.Sigma[k]
+        marginal = none_or_idx(forward_marginals, k)  # marginal at k
+        kernel = none_or_idx(forward_markov.kernels, k)  # kernel k+1 | k
+        next_marginal = none_or_idx(forward_marginals, k+1)  # marginal at k+1
 
-        a = m
-        b = fwd_d + fwd_F @ m
+        _kernel = get_reverse_kernel(marginal, kernel, next_marginal)
 
-        A = P
-        C = P.T @ fwd_F.T
-        B = fwd_F @ P @ fwd_F.T + fwd_Sigma
-
-        rvs_F = jsc.linalg.solve(B, C.T).T
-        rvs_d = a - C @ jsc.linalg.solve(B, b)
-        rvs_Sigma = A - C @ jsc.linalg.solve(B, C.T)
-
-        rvs_Fs = rvs_Fs.at[k].set(rvs_F)
-        rvs_ds = rvs_ds.at[k].set(rvs_d)
-        rvs_Sigmas = rvs_Sigmas.at[k].set(rvs_Sigma)
-
-    m = forward_marginals.mean[-1]
-    P = forward_marginals.cov[-1]
+        Fs = Fs.at[k].set(_kernel.F)
+        ds = ds.at[k].set(_kernel.d)
+        Sigmas = Sigmas.at[k].set(_kernel.Sigma)
 
     reverse_markov = GaussMarkov(
-        marginal=Gaussian(m, P),
+        Gaussian(
+            mean=forward_marginals.mean[-1],
+            cov=forward_marginals.cov[-1],
+        ),
         kernels=AffineGaussian(
-            F=rvs_Fs, d=rvs_ds, Sigma=rvs_Sigmas
+            F=Fs, d=ds, Sigma=Sigmas
         )
     )
     return reverse_markov
+
+
+def get_pairwise_marginal(
+    marginal: Gaussian,
+    kernel: AffineGaussian
+):
+    m, P = marginal
+    F, d, Sigma = kernel
+
+    q = Gaussian(
+        mean=jnp.hstack((F @ m + d, m)),
+        cov=jnp.vstack((
+            jnp.hstack((F @ P @ F.T + Sigma, F @ P)),
+            jnp.hstack((P.T @ F.T, P))
+        ))
+    )
+    return q
+
+
+def get_conditional(
+    marginal: Gaussian,
+    pairwise: Gaussian
+):
+    dim = marginal.mean.shape[0]
+
+    a = pairwise.mean[:dim]
+    b = pairwise.mean[dim:]
+
+    A = pairwise.cov[:dim, :dim]
+    B = pairwise.cov[dim:, dim:]
+    C = pairwise.cov[:dim, dim:]
+
+    return AffineGaussian(
+        F=jsc.linalg.solve(A, C).T,
+        d=b - C.T @ jsc.linalg.solve(A, a),
+        Sigma=B - C.T @ jsc.linalg.solve(A, C)
+    )
+
+
+def get_reverse_kernel(
+    marginal: Gaussian,
+    kernel: AffineGaussian,
+    next_marginal: Gaussian
+):
+    pairwise = get_pairwise_marginal(marginal, kernel)
+    return get_conditional(next_marginal, pairwise)
+
+
+def merge_messages(
+    fwd_message: Potential,
+    bwd_message: LogConditionalNorm,
+) -> Potential:
+    return Potential(
+        R=fwd_message.R + bwd_message.S,
+        r=fwd_message.r + bwd_message.s,
+        rho=fwd_message.rho + bwd_message.xi,
+    )
+
+
+def log_to_std_form(
+    potential: Potential
+) -> Gaussian:
+    return Gaussian(
+        mean=jsc.linalg.inv(potential.R) @ potential.r,
+        cov=jsc.linalg.inv(potential.R)
+    )
